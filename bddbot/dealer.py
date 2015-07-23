66c27c4a0524dcf3731f54b0e4cdd66de0e471e3
Niv Ben-David <nivbend@gmail.com>
2015-07-24 00:21:26 +0300

    Handle multiple scenarios in initialization

--- bddbot/dealer.py
+++ bddbot/dealer.py
@@ -1,17 +1,54 @@
 from os import mkdir, getcwd
+import re
 from errors import BotError
 
+REGEX_SCENARIO_START = re.compile(r"^\s*Scenario:", re.MULTILINE)
+
+def _split_bank(bank):
+    # Iterate over every occurence of 'Scenario:' and add everything from its line
+    # to the beginning of the next occurence.
+    # Everything until the start of the first occurence is considered the feature's
+    # header.
+    previous_start = None
+    feature = ""
+    scenarios = []
+    for match in REGEX_SCENARIO_START.finditer(bank):
+        current_start = match.start()
+
+        # If on the first iteration, consider it the feature's header.
+        if previous_start is None:
+            feature = bank[:current_start]
+        else:
+            scenarios.append(bank[previous_start:current_start])
+
+        previous_start = current_start
+
+    # If no scenarios were found in bank, return the whole bank.
+    if previous_start is None:
+        return (bank, [])
+
+    # Add the last scenario.
+    scenarios.append(bank[current_start:])
+
+    return (feature, scenarios)
+
 class Dealer(object):
+    def __init__(self):
+        self.__feature = ""
+        self.__scenarios = []
+
     def assign(self):
         try:
             with open("features.bank", "rb") as bank_input:
-                whole_bank = bank_input.read()
+                (self.__feature, self.__scenarios) = _split_bank(bank_input.read())
         except IOError:
             raise BotError("No features bank in {:s}".format(getcwd()))
 
-        if not whole_bank:
+        if not self.__feature:
             print("No more scenarios to deal")
 
         mkdir("features")
         with open("features/all.feature", "wb") as features:
-            features.write(whole_bank)
+            features.write(self.__feature)
+            if self.__scenarios:
+                features.write(self.__scenarios[0])
--- features/initialize.feature
+++ features/initialize.feature
@@ -44,3 +44,33 @@ Feature: Initialize Bot
                     When something happens
                     Then stuff will go down
             """
+
+    Scenario: One feature, three scenarios
+        Given the features bank:
+            """
+            Feature: A single feature to test with
+                Scenario: First scenario
+                    Given some preconditions
+                    When something happens
+                    Then stuff will go down
+
+                Scenario: Second scenario
+                    Given some more preconditions
+                    When another thing happens
+                    Then more stuff will go down
+
+                Scenario: Third scenario
+                    Given some quaky preconditions
+                    And more preconditions
+                    When all the chips have fallen
+                    Then will the fat lasy sing
+            """
+        When we first assign a scenario
+        Then "features/all.feature" contains:
+            """
+            Feature: A single feature to test with
+                Scenario: First scenario
+                    Given some preconditions
+                    When something happens
+                    Then stuff will go down
+            """
