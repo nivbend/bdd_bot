16cfae84dfac3bde625e6789f175af946ff34cfb
Niv Ben-David <nivbend@gmail.com>
2015-07-26 13:37:54 +0300

    Add packaging

---
+++ README.md
@@ -0,0 +1,8 @@
+bdd-bot
+-------
+
+An automatic BDD scenarios delivery system.
+
+This project is a sandbox for me to experiment with BDD. The idea is to have a central
+bank of features and scenarios and whenever a developer implements a scenario they are
+dealt another scenario to work on automatically.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "bdd_bot",
+    version = "0.1",
+    description = "An automatic BDD scenarios delivery system",
+    url = "http://github.com/nivbend/bdd_bot",
+    author = "Niv Ben-David",
+    author_email = "nivbend@gmail.com",
+    license = "MIT",
+    packages = ["bddbot", ])
