8a5a390732a6b2162a050287a69eadcfb4d82768
Niv Ben-David <nivbend@gmail.com>
2015-07-30 15:03:53 +0300

    Organize and fix tests for deal() errors

--- bddbot/test/test_dealer.py
+++ bddbot/test/test_dealer.py
@@ -1,5 +1,5 @@
 from cStringIO import StringIO
-from nose.tools import assert_raises, assert_equal
+from nose.tools import assert_raises, assert_in, assert_equal
 from mock import ANY, DEFAULT, MagicMock, mock_open, patch
 from bddbot import Dealer
 from bddbot.dealer import BotError
@@ -63,9 +63,10 @@ class TestLoading(BaseDealerTest):
         self.mocked_open.side_effect = IOError()
 
         dealer = Dealer()
-        with assert_raises(BotError):
+        with assert_raises(BotError) as error_context:
             dealer.load()
 
+        assert_in("no features bank", error_context.exception.message.lower())
         self.mocked_open.assert_called_once_with(FEATURE_BANK_FILENAME, "rb")
 
     def test_successful_call(self):
@@ -81,43 +82,59 @@ class TestLoading(BaseDealerTest):
 
         self.mocked_open.assert_called_once_with(FEATURE_BANK_FILENAME, "rb")
 
-class TestOutput(BaseDealerTest):
-    CONTENT = "Feature: Stuff"
+class TestDealFirst(BaseDealerTest):
+    DEFAULT_CONTENTS = [
+        "Feature: An awesome feature",
+        "  Scenario: Scenario A",
+        "  Scenario: Scenario B",
+        ]
 
     def setup(self):
         self._capture_stdout()
-        self._mock_dealer_functions(content = self.CONTENT)
-        self.dealer = self._load_dealer()
 
     def teardown(self):
         patch.stopall()
 
+    def test_cant_open_features_file_for_writing(self):
+        self._mock_dealer_functions(content = "\n".join(self.DEFAULT_CONTENTS))
+        dealer = self._load_dealer()
+
+        self.mocked_open.side_effect = [IOError(), DEFAULT, ]
+        with assert_raises(BotError) as error_context:
+            dealer.deal()
+
+        # Couldn't open file for writing, so obviously no writes were perfomed.
+        assert_in("couldn't write", error_context.exception.message.lower())
         self.mocked_open.assert_any_call(OUTPUT_FEATURES_FILENAME, "wb")
+        self.mocked_open().write.assert_not_called()
+        self.mocked_mkdir.assert_called_once_with(FEATURES_DIRECTORY)
+        assert_equal("", self.stdout)
+
+    def test_cant_write_to_feature_file(self):
+        self._mock_dealer_functions(content = "\n".join(self.DEFAULT_CONTENTS))
+        dealer = self._load_dealer()
+
+        self.mocked_open().write.side_effect = IOError()
+        with assert_raises(BotError) as error_context:
+            dealer.deal()
+
+        # First call to write() raised an IOError which was caught and translated.
+        assert_in("couldn't write", error_context.exception.message.lower())
+        self.mocked_open.assert_any_call(OUTPUT_FEATURES_FILENAME, "wb")
+        self.mocked_open().write.assert_called_once_with(ANY)
         self.mocked_mkdir.assert_called_once_with(FEATURES_DIRECTORY)
         assert_equal("", self.stdout)
 
     def test_features_directory_already_exists(self):
-        self.mocked_mkdir.side_effect = OSError()
+        self._mock_dealer_functions(content = "\n".join(self.DEFAULT_CONTENTS))
+        dealer = self._load_dealer()
 
-        self.dealer.deal()
+        self.mocked_mkdir.side_effect = OSError()
+        dealer.deal()
 
         # If directory already exist, we should proceed as usual.
-        self.mocked_open().write.assert_any_call(self.CONTENT)
-
-    def test_cant_open_features_file_for_writing(self):
-        self.mocked_open.side_effect = [IOError(), DEFAULT, ]
-
-        with assert_raises(BotError):
-            self.dealer.deal()
-
-        # Couldn't open file for writing, so obviously no writes were perfomed.
-        self.mocked_open().write.assert_not_called()
-
-    def test_cant_write_to_feature_file(self):
-        self.mocked_open().write.side_effect = IOError()
-
-        with assert_raises(BotError):
-            self.dealer.deal()
-
-        # First call to write() raised an IOError which was caught and translated.
-        self.mocked_open().write.assert_called_once_with(ANY)
+        self.mocked_open.assert_any_call(OUTPUT_FEATURES_FILENAME, "wb")
+        self.mocked_open().write.assert_any_call(self.DEFAULT_CONTENTS[0] + "\n")
+        self.mocked_open().write.assert_any_call(self.DEFAULT_CONTENTS[1] + "\n")
+        self.mocked_mkdir.assert_called_once_with(FEATURES_DIRECTORY)
+        assert_equal("", self.stdout)
